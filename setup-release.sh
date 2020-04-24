02f99c7ed419722749dcbe39140ce429415c4155
ivan.yivanov <ivan.yivanov@scalefocus.com>
2020-04-24 11:07:36 +0300

    create fix bash script - bad substitution

--- setup-release.sh
+++ setup-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-echo $( secrets.GOOGLE_JSON_KEY ) > google-services.json
+echo "${secrets.GOOGLE_JSON_KEY}" > google-services.json
 mkdir -p app/src/release
 cp google-services.json app/src/release/google-services.json
